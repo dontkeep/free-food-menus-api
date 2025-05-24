fab490359dd070d5a40a80b483215ce443ebf4f5
dontkeep <alsdiaryy@gmail.com>
2025-05-24 21:04:29 +0700

    Removed unnecessary files

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json"); 
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port,()=>console.log(`server is listeneing on port ${port}`));
+server.listen(port,()=>console.log(`server is listening on port ${port}`));
